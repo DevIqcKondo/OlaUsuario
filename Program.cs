0d3f044101b8e3a7bb81a54cfe6c5c15446a29b3
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-19 21:32:10 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.Write("Digite seu nome: ");
+            string textoDigitado = Console.ReadLine();
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.Write("Olá, ");
+            Console.Write(textoDigitado);
+            Console.WriteLine("!");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadLine();
+            Console.WriteLine("Obrigada por ajudar com meu exercício :) ");
+            Console.ResetColor();
+          
+        }
+        }
+}
